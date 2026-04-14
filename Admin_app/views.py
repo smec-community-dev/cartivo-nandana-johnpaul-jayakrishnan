b2437b64c74dd3655a98767fd5638d0f7b977462
jayakrishnan <kjayakrishnan2004@gmail.com>
2026-04-14 21:20:57 +0530

    fixed remaining conflict in views.py

--- Admin_app/views.py
+++ Admin_app/views.py
@@ -9,10 +9,7 @@ from .models import Offer,Discount,OfferDiscountBridge,ProductOfferBridge,Catego
 from django.contrib import messages
 from Seller_app.models import Product
 from Seller_app.models import SellerProfile
-<<<<<<< HEAD
 
-=======
->>>>>>> jayakrishnan-dev1
 
 def admin_required(view_func):
     @wraps(view_func)
@@ -328,7 +325,7 @@ def Product_Discount_Bridge(request):
         "bridge": bridge
     })
         
-<<<<<<< HEAD
+
 
 
 
@@ -348,10 +345,10 @@ def reject_seller(request, id):
     seller = SellerProfile.objects.get(id=id)
     seller.delete()
     return redirect('pending_seller')
-=======
+
 def approve_seller(request,id):
     sellers = SellerProfile.objects.get(id=id)
     sellers.approved = True
     sellers.save()
     return redirect('pending_seller') 
->>>>>>> jayakrishnan-dev1
+
